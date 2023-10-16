fd559a86a46a67dd82bdc80e4e56515d967be96c
Vilius <viliusps@gmail.com>
2023-10-16 21:32:46 +0300

    authorization
    still need to fix securityConfig

--- recommendations/src/main/java/com/media/recommendations/RecommendationsApplication.java
+++ recommendations/src/main/java/com/media/recommendations/RecommendationsApplication.java
@@ -2,8 +2,12 @@ package com.media.recommendations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.media.recommendations.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class RecommendationsApplication {
 
 	public static void main(String[] args) {
